7e68b02fbce8e0753d5b8806b7eb64c7454d5354
Ryan Gao <ryantchgao@googlemail.com>
2023-01-06 06:43:23 -0800

    refactor footer icons

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,4 @@
-import { Flex, Box } from '@chakra-ui/react';
-import { NavLink } from 'react-router-dom';
+import { Flex } from '@chakra-ui/react';
 
 import { IoMdHome, IoMdStats, IoMdSettings } from 'react-icons/io'
 import FooterIcon from './FooterIcon'
@@ -10,33 +9,12 @@ export type ColorObject = {
 }
 
 const Footer = () => {
-
-
     return (
-        <Flex direction="row" align="center" justify="center" gap="100px" py="15px" px="30px" borderTop="1px" borderColor="#d1d1d1" marginTop="auto">
-            <FooterIcon icon={<IoMdHome size="36px"/>} path="/" />
-            <FooterIcon icon={<IoMdStats size="36px"/>} path="/statistics" />
-            <FooterIcon icon={<IoMdSettings size="36px"/>} path="/settings" />
-            
-            
-            {/* <Box
-                onMouseEnter={() => { setIsHover(true) }}
-                onMouseLeave={() => { setIsHover(false) }}>
-                <NavLink to="/" style={pickStyle}>
-                    <IoMdHome size="36px" />
-                </NavLink>
-            </Box> */}
-            {/* <NavLink to="/statistics" style={({ isActive }) =>
-                isActive ? selectedColor : unselectedColor}>
-                <IoMdStats size="36px" />
-            </NavLink>
-            <NavLink to="/settings" style={({ isActive }) =>
-                isActive ? selectedColor : unselectedColor}>
-                <IoMdSettings size="36px" />
-            </NavLink> */}
+        <Flex direction="row" align="center" justify="center" gap="100px" py="15px" px="30px" borderTop="1px" borderColor="#d1d1d1" marginTop="auto" >
+            <FooterIcon icon={<IoMdHome size="36px" />} path="/" />
+            <FooterIcon icon={<IoMdStats size="36px" />} path="/statistics" />
+            <FooterIcon icon={<IoMdSettings size="36px" />} path="/settings" />
         </Flex>
-
-
     );
 }
 
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,12 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <Flex direction="row" align="center" justify="center" gap="275px" p="15px" borderBottom="1px" borderColor="#d1d1d1">
+        <Flex direction="row" align="center" justify="center" gap="275px" p="15px" borderBottom="1px" borderColor="#d1d1d1" >
             <Box>Pomo</Box>
             <Box>Login</Box>
         </Flex>
-
-
     );
 }
 
