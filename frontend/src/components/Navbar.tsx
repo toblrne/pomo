a902a5cbb6bbac8ff5094327e6283aa9d3d68562
Ryan Gao <ryantchgao@googlemail.com>
2023-01-06 22:37:34 -0800

    implement nonfunctional timer

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <Flex direction="row" align="center" justify="center" gap="275px" p="15px" borderBottom="1px" borderColor="#d1d1d1" >
-            <Box>Pomo</Box>
+            <Box fontSize="17px" fontWeight="semibold">Pomo</Box>
             <Box>Login</Box>
         </Flex>
     );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,38 @@
 import * as React from 'react';
 
-import { Flex, Box } from "@chakra-ui/react"
+import { Flex, Box, Tab, TabList, TabPanel, TabPanels, Tabs, Button } from "@chakra-ui/react"
 import Countdown from '../components/Countdown';
 
 const Home = () => {
 
     return (
-        <Flex direction="column" align="center" justify="center" border="1px" m="25px" borderRadius="12px"> 
-            <Flex direction="row" justify="center" align="center" gap="2rem" border="1px" my="15px">
-                <Box> Pomodoro </Box>
-                <Box> Short Break </Box>
-                <Box> Long Break </Box>
-            </Flex>
+        <Flex direction="column" align="center" justify="center" border="1px" m="25px" borderRadius="12px" py="10px">
+            <Tabs variant="unstyled">
+                <TabList>
+                    <Tab color="#777777" fontSize="15px" fontWeight="semibold" _selected={{ color: "#000000" }}> Pomodoro </Tab>
+                    <Tab color="#777777" fontSize="15px" fontWeight="semibold" _selected={{ color: "#000000" }}> Short Break </Tab>
+                    <Tab color="#777777" fontSize="15px" fontWeight="semibold" _selected={{ color: "#000000" }}> Long Break </Tab>
+                </TabList>
+                <TabPanels display="flex" justifyContent="center">
+                    <TabPanel>
+                        <Flex fontSize="72px" fontWeight="medium" align="center" direction="column">
+                            <Box mb="15px">
+                                25:00
+                            </Box>
+                            <Button colorScheme='teal' size='lg' mb="10px">
+                                START
+                            </Button>
+                        </Flex>
 
-            <Countdown />
-
+                    </TabPanel>
+                    <TabPanel>
+                        <Box fontSize="72px" fontWeight="medium">5:00</Box>
+                    </TabPanel>
+                    <TabPanel>
+                        <Box fontSize="72px" fontWeight="medium">10:00</Box>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
 
 
         </Flex>
